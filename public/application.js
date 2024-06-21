41fbb547306c5ff8c8ffa4e3e2a547c2fd355363
Travis Lawrence <travis@instabase.com>
2024-06-20 18:27:38 -0700

    init commit

---
+++ public/application.js
@@ -0,0 +1,18 @@
+const BASE_URL = "https://api.setlist.fm/rest";
+const FOO_FIGHTERS_MBID = "67f66c07-6e61-4026-ade5-7e782fad3a5d";
+const API_KEY = "";
+
+const fetchSetListData = async ({ mbid, page }) => {
+  const resp = await fetch(`${BASE_URL}/1.0/artist/${mbid}/setlists`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      "x-api-key": API_KEY,
+    },
+  });
+  const data = await resp.json();
+  console.log(data);
+  return data;
+};
+
+fetchSetListData({ mbid: FOO_FIGHTERS_MBID, page: 1 });
